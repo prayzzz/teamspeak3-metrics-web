39760641e84470e6e4a560b1cb04220d85e99439
prayzzz <prayzzz@outlook.com>
2020-11-11 18:19:03 +0100

    try to don't run CI on tag push

--- release.ps1
+++ release.ps1
@@ -2,6 +2,7 @@ $minutes = ([int](Get-Date -format "HH") * 60) + (Get-Date -format "mm")
 $version = (Get-Date -format "yyyy.MM.dd") + ".$minutes"
 
 Write-Host "Tag will be $version"
+Write-Host
 
 git tag $version
 git push --tags
